cf1c43a6aa91044f2be01ac10c26169becdfddfb
Mousaibrah <mousaKibrahim@gmail.com>
2023-10-18 22:53:18 +0300

    implement upgrade handler

---
+++ app/upgrades/v2/constants.go
@@ -0,0 +1,8 @@
+package v2
+
+const (
+	// UpgradeName is the shared upgrade plan name for mainnet
+	UpgradeName = "v2"
+	// UpgradeInfo defines the binaries that will be used for the upgrade
+	UpgradeInfo = "{\"binaries\":{\"linux/amd64\":\"https://github.com/mousaibrah/ggezchain/releases/download/v2/ggezchaind\"}}"
+)
